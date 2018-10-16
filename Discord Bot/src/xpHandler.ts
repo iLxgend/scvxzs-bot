167e15607de7285bc09edc427ef96412e5ead25f
DESKTOP-BRTMTMV\Nathan <natemi1010@gmail.com>
2018-10-16 21:59:50 +0100

    Modified xp handler

--- Discord Bot/src/xpHandler.ts
+++ Discord Bot/src/xpHandler.ts
@@ -8,14 +8,14 @@ import { receiveXp } from './models/receiveXp.js';
 export class xpHandler {
     private _config: api.IBotConfig;
 
-    constructor(config: api.IBotConfig){
+    constructor(config: api.IBotConfig) {
         this._config = config;
     }
 
-    public async IncreaseXpOnMessage(message: discord.Message){
+    public async IncreaseXpOnMessage(message: discord.Message) {
         let userXpURL = 'https://api.dapperdino.co.uk/api/xp/' + message.author.id;
 
-        let xpObject:postXp = new postXp();
+        let xpObject: postXp = new postXp();
         let xpValue = Math.floor(Math.random() * 10) + 5;
         xpObject.xp = xpValue;
         xpObject.DiscordId = message.author.id;
@@ -24,10 +24,10 @@ export class xpHandler {
         new apiRequestHandler().RequestAPI("POST", xpObject, userXpURL, this._config)
     }
 
-    public async IncreaseXp(message: discord.Message, xp: number){
+    public async IncreaseXp(message: discord.Message, xp: number) {
         let userXpURL = 'https://api.dapperdino.co.uk/api/xp/' + message.author.id;
 
-        let xpObject:postXp = new postXp();
+        let xpObject: postXp = new postXp();
         xpObject.xp = xp;
         xpObject.DiscordId = message.author.id;
         xpObject.Username = message.author.username;
@@ -35,22 +35,27 @@ export class xpHandler {
         new apiRequestHandler().RequestAPI("POST", xpObject, userXpURL, this._config)
     }
 
-    public async GetXp() {
-        let xpUrl = 'https://api.dapperdino.co.uk/api/xp'
+    private LevelUp() {
         
-        new apiRequestHandler().RequestAPI("GET", null, xpUrl, this._config)
-        .then((xpArray) => {
-            console.log(xpArray);
-        });
     }
 
-    public async GetXpById(discordId:number) {
-        let xpUrl = `https://api.dapperdino.co.uk/api/xp/${discordId}`
-        
+    public async GetXp() {
+        let xpUrl = 'https://api.dapperdino.co.uk/api/xp'
+
         new apiRequestHandler().RequestAPI("GET", null, xpUrl, this._config)
-        .then((xpReturnObject) => {
-            console.log(xpReturnObject);
-            //actual object is xpReturnObject.data 
-        });
+            .then((xpArray) => {
+                console.log(xpArray);
+            });
+    }
+
+    public async GetXpById(discordId: number) {
+        return new Promise<number>(async (resolve, reject) => {
+            let xpUrl = `https://api.dapperdino.co.uk/api/xp/${discordId}`
+
+            new apiRequestHandler().RequestAPI("GET", null, xpUrl, this._config)
+                .then((xpReturnObject) => {
+                    return resolve(xpReturnObject.data.xp);
+                });
+        })
     }
 }
