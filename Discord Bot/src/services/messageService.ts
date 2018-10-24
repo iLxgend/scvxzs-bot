42b41f487de751386c133099fb9174e38439dd38
Mick Rustenhoven <rustenhovenmick@outlook.com>
2018-10-24 02:51:55 +0200

    Added comments to messageService

--- Discord Bot/src/services/messageService.ts
+++ Discord Bot/src/services/messageService.ts
@@ -16,35 +16,63 @@ export class messageService {
 
     public HandleMessageInTicketCategory(message: discord.Message) {
 
+        // Get ticket channel id from channel name
         let ticketChannelId = ((message.channel as discord.TextChannel).name.toString().replace("ticket", "")).toString();
-        if (message.content == "?closeTicket") {
 
-            new apiRequestHandler().requestAPIWithType<ticketReceive>('GET', null, `https://api.dapperdino.co.uk/api/ticket/${ticketChannelId}`, this._config)
+        // Check if content is equal to ?closeTicket
+        if (message.content === "?closeTicket") {
+
+            // Request API
+            new apiRequestHandler()
+                
+                // Request with type so in our .then method we'll have intellisense
+                .requestAPIWithType<ticketReceive>('GET', null, `https://api.dapperdino.co.uk/api/ticket/${ticketChannelId}`, this._config)
+
+                // All went okay
                 .then(ticketReceive => {
-                    let ticketChannelUser = ticketReceive.applicant.discordId;
 
-                    if(message.author.id == ticketChannelUser){
+                    // Get ticket channel creator
+                    let creatorId = ticketReceive.applicant.discordId;
+
+                    // Check if current user is creator
+                    if (message.author.id == creatorId) {
+
+                        // Delete message
                         message.channel.delete();
+
+                        // Close ticket through API
                         new apiRequestHandler().requestAPI('POST', null, `https://api.dapperdino.co.uk/api/ticket/${ticketChannelId}/close`, this._config);
                     }
-                    else{
+                    else 
+                    {
+                        // Delete message if it's not the creator
                         message.delete(0);
+
+                        // Create embed that tells the creator to close the ticket
                         let endTicketEmbed = new discord.RichEmbed()
                             .setTitle(`${message.author.username} thinks that this ticket can be closed now`)
                             .setThumbnail(message.author.avatarURL)
                             .setColor("#2dff2d")
                             .setDescription("If you agree that this ticket should be closed then please type the following command:\n__**?closeTicket**__")
+
+                        // Send embed to ticket channel
                         message.channel.send(endTicketEmbed);
                     }
-                })
-                return;
+                });
+
+            return;
         }
+
+        // Create new ticket reaction if no commands were used
         let reaction = new ticketReaction();
+
+        // Fill ticket reaction model
         reaction.ticketId = parseInt(ticketChannelId);
         reaction.fromId = message.author.id;
         reaction.message = message.content;
         reaction.messageId = message.id;
 
+        // Request API and add our reaction to the database.
         new apiRequestHandler().requestAPI('POST', reaction, 'https://api.dapperdino.co.uk/api/ticket/reaction', this._config);
     }
 }
