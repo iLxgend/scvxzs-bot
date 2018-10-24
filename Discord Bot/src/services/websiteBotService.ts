07c737fe02571206d4752d7779e5b5fd49aaf32e
Mick Rustenhoven <rustenhovenmick@outlook.com>
2018-10-24 02:42:56 +0200

    Added comments to website bot service

--- Discord Bot/src/services/websiteBotService.ts
+++ Discord Bot/src/services/websiteBotService.ts
@@ -22,15 +22,18 @@ export class websiteBotService {
 
     startupService = () => {
 
+        // Creates connection to our website's SignalR hub
         const connection = new aspnet.HubConnectionBuilder()
             .withUrl('https://dapperdino.co.uk/discordbothub')
             .configureLogging(aspnet.LogLevel.Debug)
             .build();
 
+        // Starts connection
         connection.start()
             .then(() => console.log("t"))
             .catch(err => console.error(err.toString()));
 
+        // On 'ReceiveMessage' -> test method
         connection.on("ReceiveMessage", (user, message) => {
             let testUser = this._serverBot.users.get(this.GetDiscordUserByUsername(user).discordId);
             if (testUser) {
@@ -40,48 +43,87 @@ export class websiteBotService {
             }
         });
 
+        // On 'SuggestionUpdate' -> fires when suggestion is updated on the website
         connection.on("SuggestionUpdate", (suggestion) => {
-            let testUser = this._serverBot.users.get(suggestion.discordUser.discordId);
-            if (testUser) {
+
+            // Get user that suggested this suggestion
+            let suggestor = this._serverBot.users.get(suggestion.discordUser.discordId);
+
+            // Check if found
+            if (suggestor) {
+
+                // Create suggestion embed
                 let suggestionUpdateEmbed = new discord.RichEmbed()
                     .setTitle("Your suggestion has been updated!")
                     .setColor("0xff0000")
                     .addField("Here you will find the information about your updated suggestion:", `https://dapperdino.co.uk/Client/Suggestion/${suggestion.id}`)
                     .addField("Thanks as always for being a part of the community, it means a lot", "")
-                    .setFooter("With ❤ By the DapperCoding team")
-                testUser.send(suggestionUpdateEmbed)
+                    .setFooter("With ❤ By the DapperCoding team");
+
+                // Send embed to suggestor
+                suggestor.send(suggestionUpdateEmbed)
                     .catch(console.error)
             }
         });
 
+        // On 'FaqUpdate' -> fires when faq is updated on the website
         connection.on("FaqUpdate", async (faq) => {
+
+            // Get FAQ channel
             let faqChannel = this._serverBot.channels.get("461486560383336458");
 
+            // If FAQ channel is found
             if (faqChannel) {
+
+                // Get as text channel
                 let channel = faqChannel as discord.TextChannel;
+
+                // Try to find message with id of updated faq item
                 let message = await channel.fetchMessage(faq.messageId);
+
+                // Try to delete message, then add the updated version
                 message
                     .delete()
                     .then(() => {
+
+                        // Create faq embed
                         let faqEmbed = new discord.RichEmbed()
                             .setTitle("-Q: " + faq.question)
                             .setDescription("-A: " + faq.answer)
                             .setColor("#2dff2d")
+
+                        // Check if resource link is present
                         if (faq.resourceLink != null) {
+
+                            // Add resource link to faq embed
                             faqEmbed.addField("Useful Resource: ", "[" + faq.resourceLink.displayName + "](" + faq.resourceLink.link + ")");
                         }
-                        channel.send(faqEmbed).then((newMsg) => this.SetFaqMessageId((newMsg as discord.Message).id, faq.id, this._config));
+
+                        // Send updated version of embed
+                        channel
+                            .send(faqEmbed)
+                            .then((newMsg) => {
+
+                                // Set FAQ message id in db through api when updated
+                                this.SetFaqMessageId((newMsg as discord.Message).id, faq.id, this._config)
+                            });
                     })
                     .catch(console.error);
             }
         });
     }
 
+    // Sets faq message in the database through our API
     private SetFaqMessageId(messageId: string, faqId: number, config: api.IBotConfig) {
+
+        // Create new faqMessage object
         let faqMessageObject = new faqMessage();
+
+        // Fill with faq & message id
         faqMessageObject.id = faqId;
         faqMessageObject.messageId = messageId;
 
+        // Request API
         new apiRequestHandler().requestAPI("POST", faqMessageObject, 'https://api.dapperdino.co.uk/api/faq/AddMessageId', config)
     }
 
@@ -108,48 +150,70 @@ export class websiteBotService {
         return usersWithRole;
     }
 
+    // Get server population
     public GetServerPopulation() {
+
+        // Return length of members array
         return this._server.members.array().length;
     }
 
+    // Get discord user by username
     public GetDiscordUserByUsername(username: string) {
-        let allUsers = this._serverBot.users.array();
-        let user;
-        for (let i = 0; i < allUsers.length; i++) {
 
-            if (allUsers[i].username == username) {
-                user = allUsers[i];
-                break;
-            }
-        }
+        // Try to find user by username
+        let user = this._serverBot.users.find(user => user.username === username);
+
+        // Create compact discord user
         let userObject = new compactDiscordUser()
+
+        // Doesn't fill if user couldn't be found
         if (user != null) {
+
+            // Fills userObject if user is found
             userObject.username = user.username;
             userObject.discordId = user.id;
         }
+
+        // Returns compact discord user that's either empty or filled with the information gotten from the server
         return userObject;
     }
 
+    // Get discord user by id
     public GetDiscordUserById(id: string) {
-        let allUsers = this._serverBot.users.array();
-        let user;
-        for (let i = 0; i < allUsers.length; i++) {
-            if (allUsers[i].id == id) {
-                user = allUsers[i];
-                break;
-            }
-        }
-        let userObject = new compactDiscordUser()
-        userObject.username = user.username;
-        userObject.discordId = user.id;
 
+        // Try to find user by id    
+        let user = this._serverBot.users.find(user => user.id === id);
+
+        // Create compact discord user
+        let userObject = new compactDiscordUser()
+
+        // Doesn't fill if user couldn't be found
+        if (user != null) {
+
+            // Fills userObject if user is found
+            userObject.username = user.username;
+            userObject.discordId = user.id;
+        }
+
+        // Returns compact discord user that's either empty or filled with the information gotten from the server
         return userObject;
     }
 
+    // Get discord user by email from API
     public GetDiscordUserByEmail(emailAddress: string) {
+
+        // Create new Email object
         let emailObject = new email();
+
+        // Add email address to it
         emailObject.Email = emailAddress;
+
+        // Get response from api
         let responseData = new apiRequestHandler().requestAPI("POST", emailObject, "https://dapperdinoapi.azurewebsites.net/api/search/user", this._config);
+
+        // Try to log data
         console.log(responseData);
+
+        /// THIS METHOD NEEDS TO BE REFACTORED
     }
 }
