a4d26c86f6a1658f9f66598f0c3b5161bf317f4f
Mick Rustenhoven <rustenhovenmick@outlook.com>
2019-04-15 05:04:28 +0200

    Added paged rich embed to commands

--- Discord Bot/src/commands/commands.ts
+++ Discord Bot/src/commands/commands.ts
@@ -1,69 +1,143 @@
-import { IBot, IBotCommand, IBotCommandHelp, IBotMessage, IBotConfig } from '../api'
-import { getRandomInt } from '../utils'
-import * as discord from 'discord.js'
+import {
+  IBot,
+  IBotCommand,
+  IBotCommandHelp,
+  IBotMessage,
+  IBotConfig
+} from "../api";
+import { getRandomInt } from "../utils";
+import * as discord from "discord.js";
+import { GenericRichEmbedPageHandler } from "../genericRichEmbedPageHandler";
 
 export default class CommandsCommand implements IBotCommand {
-    private readonly CMD_REGEXP = /^\?commands/im
+  private readonly CMD_REGEXP = /^\?commands/im;
 
-    public getHelp(): IBotCommandHelp {
-        return { caption: '?commands', description: 'Sends you a list of all our commands, that\'ts how you got here in the first place' }
+  public getHelp(): IBotCommandHelp {
+    return {
+      caption: "?commands",
+      description:
+        "Sends you a list of all our commands, that'ts how you got here in the first place"
+    };
+  }
+
+  public init(bot: IBot, dataPath: string): void {}
+
+  public isValid(msg: string): boolean {
+    return this.CMD_REGEXP.test(msg);
+  }
+
+  public canUseInChannel(channel: discord.TextChannel): boolean {
+    return !channel.name.toLowerCase().startsWith("ticket");
+  }
+
+  public canUseCommand(roles: discord.Role[]) {
+    let helpObj: IBotCommandHelp = this.getHelp();
+    let canUseCommand = true;
+
+    if (helpObj.roles != null && helpObj.roles.length > 0) {
+      canUseCommand = false;
+
+      for (var cmdRole in helpObj.roles) {
+        if (
+          roles.find(role => role.name.toLowerCase() == cmdRole.toLowerCase())
+        )
+          canUseCommand = true;
+      }
     }
 
-    public init(bot: IBot, dataPath: string): void { }
+    return canUseCommand;
+  }
 
-    public isValid(msg: string): boolean {
-        return this.CMD_REGEXP.test(msg)
-    }
+  public async process(
+    msg: string,
+    answer: IBotMessage,
+    msgObject: discord.Message,
+    client: discord.Client,
+    config: IBotConfig,
+    commands: IBotCommand[]
+  ): Promise<void> {
+    let embed = new discord.RichEmbed()
+      .setTitle("Here is a list of all our commands")
+      .setColor("#ff0000");
 
-    public canUseInChannel(channel: discord.TextChannel): boolean {
-        return !channel.name.toLowerCase().startsWith("ticket");
-    }
+    msgObject.author.send(embed).then(async message => {
+      // TypeScript hack for sending a single message
+      if (Array.isArray(message)) {
+        message = message[0];
+      }
 
-    public canUseCommand(roles: discord.Role[]) {
-        let helpObj: IBotCommandHelp = this.getHelp();
-        let canUseCommand = true;
+      // React with possible reactions
+      await message.react("◀");
+      await message.react("▶");
 
-        if (helpObj.roles != null && helpObj.roles.length > 0) {
-            canUseCommand = false;
+      let itemHandler = (embed: discord.RichEmbed, data: IBotCommand[]) => {
+        data.forEach(item => {
+          let helpObj = item.getHelp();
 
-            for (var cmdRole in helpObj.roles) {
-                if (roles.find(role => role.name.toLowerCase() == cmdRole.toLowerCase()))
-                    canUseCommand = true;
-            }
-        }
-
-        return canUseCommand;
-    }
-
-    public async process(msg: string, answer: IBotMessage, msgObj: discord.Message, client: discord.Client, config: IBotConfig, commands: IBotCommand[]): Promise<void> {
-        let helpEmbed = new discord.RichEmbed()
-            .setTitle("Here is a list of all our commands")
-            .setColor("#ff0000");
-        let index = 0;
-        for (const cmd of commands) {
-            let helpObj = cmd.getHelp();
-
-            if (cmd.canUseCommand(msgObj.member.roles.array())) {
-                helpEmbed.addField(helpObj.caption, helpObj.description, false);
-                if (index % 20 == 0) {
-                    msgObj.author.send(helpEmbed);
-                    helpEmbed = new discord.RichEmbed()
-                        .setTitle("Here is a list of all our commands")
-                        .setColor("#ff0000");
-                }
-                index++;
-            }
-
-
-        }
-        msgObj.author.send(helpEmbed);
-        let confirmationEmbed = new discord.RichEmbed()
-            .setTitle("Hello " + msgObj.author.username)
-            .setColor("#ff0000")
-            .addField("I've just sent you a pm with all the server's commands", "I hope you enjoy your time here and make the most out of me, DapperBot", false)
-        msgObj.channel.send(confirmationEmbed).then(newMsg => {
-            msgObj.delete(0);
-            (newMsg as discord.Message).delete(5000);
+          if (item.canUseCommand(msgObject.member.roles.array())) {
+            embed.addField(helpObj.caption, helpObj.description, false);
+          }
         });
-    }
+
+        return embed;
+      };
+
+      // Create actual handler
+      let handler = new GenericRichEmbedPageHandler<IBotCommand>(
+        commands,
+        5,
+        itemHandler,
+        embed,
+        message as discord.Message
+      );
+
+      handler.showPage();
+
+      const filter = (reaction: discord.MessageReaction, user: discord.User) =>
+        // Check if emoji is ◀ or ▶
+        (reaction.emoji.name === "◀" || reaction.emoji.name === "▶") &&
+        // Check if reaction is added by command user
+        user.id === msgObject.author.id;
+
+      // Create a new collector for the message,
+      const collector = message.createReactionCollector(filter, {
+        time: 60 * 1000
+      });
+
+      // Will hit each time a reaction is collected
+      collector.on("collect", r => {
+        if (r.emoji.name === "◀") {
+          handler.PreviousPage();
+        } else if (r.emoji.name === "▶") {
+          handler.NextPage();
+        }
+
+        // Loop over all users for this reaction
+        r.users.forEach(user => {
+          // Check if user isn't a bot
+          if (!user.bot) {
+            // remove reaction for use
+            r.remove(user);
+          }
+        });
+      });
+
+      collector.on("end", collected => {
+        collected.first().message.delete(0);
+      });
+    });
+
+    let confirmationEmbed = new discord.RichEmbed()
+      .setTitle("Hello " + msgObject.author.username)
+      .setColor("#ff0000")
+      .addField(
+        "I've just sent you a pm with all the server's commands",
+        "I hope you enjoy your time here and make the most out of me, DapperBot",
+        false
+      );
+    msgObject.channel.send(confirmationEmbed).then(newMsg => {
+      msgObject.delete(0);
+      (newMsg as discord.Message).delete(5000);
+    });
+  }
 }
---
+++ Discord Bot/src/genericRichEmbedPageHandler.ts
@@ -0,0 +1,80 @@
+import { RichEmbed, Message } from "discord.js";
+
+export class GenericRichEmbedPageHandler<T> {
+  data: T[];
+  itemsPerPage: number;
+  itemHandler: (embed: RichEmbed, data: T[]) => RichEmbed;
+  currentPage: number = 1;
+  amountOfPages: number;
+  embed: RichEmbed;
+  message: Message;
+
+  /**
+   *
+   */
+  constructor(
+    data: T[],
+    itemsPerPage: number,
+    itemHandler: (embed: RichEmbed, data: T[]) => RichEmbed,
+    embed: RichEmbed,
+    message: Message
+  ) {
+    this.data = data;
+    this.itemsPerPage = itemsPerPage;
+    this.itemHandler = itemHandler;
+    this.embed = embed;
+    this.message = message;
+
+    let totalPages = Math.floor(data.length / itemsPerPage);
+
+    if (data.length % itemsPerPage != 0) {
+      totalPages++;
+    }
+
+    this.amountOfPages = totalPages;
+  }
+
+  public NextPage = () => {
+    if (this.currentPage == this.amountOfPages) {
+      this.currentPage = 1;
+    } else {
+      this.currentPage++;
+    }
+
+    this.showPage();
+  };
+
+  public PreviousPage = () => {
+    if (this.currentPage == 1) {
+      this.currentPage = this.amountOfPages;
+    } else {
+      this.currentPage--;
+    }
+
+    this.showPage();
+  };
+
+  public showPage() {
+    // Get the start index, if page == 1, the index is 0
+    let start =
+      this.currentPage == 1 ? 0 : (this.currentPage - 1) * this.itemsPerPage;
+
+    // Get the end index
+    let end =
+      this.itemsPerPage * this.currentPage > this.data.length
+        ? this.data.length
+        : this.itemsPerPage * this.currentPage;
+
+    // Get the section of the data you want to show
+    let data = this.data.slice(start, end);
+
+    // Reset fields
+    this.embed.fields = [];
+
+    // Set embed fields
+    this.embed = this.itemHandler(this.embed, data);
+
+    // Update the message
+    this.message.edit(this.embed);
+  }
+}
